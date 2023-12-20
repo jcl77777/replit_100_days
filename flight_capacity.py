962a2cfb8829b15abd84ed9748179e08452a0931
jcl77777 <146434729+jcl77777@users.noreply.github.com>
2023-12-20 11:35:23 +0800

    add decorator function_announce

--- flight_capacity.py
+++ flight_capacity.py
@@ -1,3 +1,12 @@
+def announce(f):
+    #Modified wrapper to accept *args (Arbitrary Positional Arguments) and **kwargs (Arbitrary Keyword Arguments). using both accept any combination of positional and keyword arguments.
+    def wrapper(*args, **kwargs):
+        print("About to check the flight availability...")
+        result = f(*args, **kwargs)
+        print("Done with checking.")
+        return result
+    return wrapper
+
 class Flight():
     #define the primary variable: capacity and passenger list. Goal is to ensure the flight would not be overbooked. 
     def __init__(self, capacity):
@@ -5,6 +14,7 @@ class Flight():
         self.passengers = []
 
     #for every passenger to book the flight, if capacity is allowed, add to passenger list. if not, return not allowed messages. 
+    @announce
     def add_passenger(self, name):
         # if self.open_seats == 0:
         if not self.open_seats():
