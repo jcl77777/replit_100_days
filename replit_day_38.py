8c06e8db2b9cfbc2eb5258aa53ccdcf0396d7247
jcl77777 <146434729+jcl77777@users.noreply.github.com>
2023-12-29 20:15:42 +0800

    Create replit_day_38.py

---
+++ replit_day_38.py
@@ -0,0 +1,21 @@
+def colorChange(color):
+  if color=="r":
+    print("\033[31m", end="")
+  elif color==" ":
+    print("\033[0m", end="")
+  elif color=="b":
+    print("\033[34m", end="")
+  elif color=="y":
+    print("\033[33m", end="")
+  elif color == "g":
+    print("\033[32m", end="")
+  elif color == "p":
+    print("\033[35m", end="")
+
+sentence = input("What will you like to rainbow-ize?: ")
+#change the color based on the lower letter
+for letter in sentence:
+  colorChange(letter.lower())
+  #print after color change
+  print(letter, end="")
+print()
