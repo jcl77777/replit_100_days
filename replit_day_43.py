23c1124c30a6bd537c97e0263145d8564fc4b49a
jcl77777 <146434729+jcl77777@users.noreply.github.com>
2024-01-02 21:32:44 +0800

    delete unnecessary comma

--- replit_day_43.py
+++ replit_day_43.py
@@ -17,7 +17,7 @@ values.sort()
 # Create a bingo list with three sublists
 bingo_list = [
     [values[0], values[1], values[2]],
-    [values[3], "BINGO,", values[4]],
+    [values[3], "BINGO", values[4]],
     [values[5], values[6], values[7]]
 ]
 
