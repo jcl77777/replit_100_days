80269b52f4859be75191ea8776cfbd76d83a88dc
jcl77777 <146434729+jcl77777@users.noreply.github.com>
2024-01-02 21:32:06 +0800

    Create replit_day_43.py

---
+++ replit_day_43.py
@@ -0,0 +1,26 @@
+import random
+
+values = []
+
+for i in range(8):
+    number = random.randint(0, 90)
+    # numbers not repeated
+    if number in values:
+        continue
+    # numbers added to the list
+    else:
+        values.append(number)
+
+# numbers sorted
+values.sort()
+
+# Create a bingo list with three sublists
+bingo_list = [
+    [values[0], values[1], values[2]],
+    [values[3], "BINGO,", values[4]],
+    [values[5], values[6], values[7]]
+]
+
+# Print each row
+for row in bingo_list:
+    print(row)
