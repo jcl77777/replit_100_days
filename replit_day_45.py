e984c6a2f2afb6ce2baf0c85ac6cb715db6d3c73
jcl77777 <146434729+jcl77777@users.noreply.github.com>
2024-01-04 21:31:28 +0800

    Create replit_day_45.py

---
+++ replit_day_45.py
@@ -0,0 +1,106 @@
+'''
+🌟Life Organizer🌟
+
+Welcome to the life organizer. Do you want to add, view, edit or remove a to do? > Add
+
+What is the task? > Pay teachers more.
+When is it due by? > 11/01/2022
+What is the priority? >  High
+
+Thanks, this task has been added.
+
+Do you want to see the menu again or quit? > quit.
+'''
+import time
+import os
+
+toDoList = []
+priorityList = [
+    "High",
+    "Medium",
+    "Low"
+]
+def prettyPrint():
+  print()
+  for item in toDoList:
+    print(item)
+  print()
+
+def clear():
+  time.sleep(2)
+  os.system('clear')
+
+print("🌟Life Organizer🌟")
+
+while True:
+  
+  action = input("Welcome to the life organizer. Do you want to add, view, edit, remove a to do or quit? (a/v/e/r/q)> ").strip().lower()
+  #add
+  if action == "a":
+    task = input("What is the task?> ")
+    due = input("When is it due by? (DD/MM/YYYY)> ")
+    while True:
+      priority = input("What is the priority? (Low/Medium/High) > ").strip().capitalize()
+      if priority not in priorityList:
+        print("Please provide a valid priority (Low/Medium/High).")
+      else:
+        new_task = f"Task: {task}, Due: {due}, Priority: {priority}"
+        toDoList.append(new_task)
+        print("Thanks, this task has been added successfully.")
+        prettyPrint()
+        clear()
+        break
+  #view
+  elif action == "v":
+    if len(toDoList) == 0:
+      print("No tasks found.")
+    else:
+      prettyPrint()
+      clear()
+
+  #edit
+  elif action == "e":
+    prettyPrint()
+    task = input("Which task would you like to edit? > ").strip()
+    for i in range(len(toDoList)):
+      if task in toDoList[i]:
+        update_task = input("What is the new task? > ").strip()
+        update_due = input("When is it due by? (DD/MM/YYYY) > ").strip()
+        while True:
+           update_priority = input("What is the new priority? (Low/Medium/High) > ").strip().capitalize()
+           if update_priority in priorityList:
+              break
+           else:
+              print("Please provide a valid priority (Low/Medium/High).")
+              continue
+        toDoList[i] = f"Task: {update_task}, Due: {update_due}, Priority: {update_priority}"
+        print("Task updated successfully.")
+        prettyPrint()
+        clear()
+        break
+      else:
+        print("Task not found.")
+
+  
+  #remove
+  elif action == "r":
+    prettyPrint()
+    task = input("Which task you would like to remove? > ")
+    #check each item in the list
+    if task in toDoList:
+      toDoList.remove(task)
+      print("task removed successfully.")
+      prettyPrint()
+      clear()
+    else:
+      print("Task not found.")
+      clear()
+  #quit
+  elif action == "q":
+    print("Thanks for using the life organizer. Goodbye!")
+    break
+  
+  #invalid input
+  else:
+    print("Invalid action. Please choose a/v/e/r/q.")
+    clear()
