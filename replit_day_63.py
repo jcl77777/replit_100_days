013e8650fde496f3ee926eaeb6ea36a4607875d5
jcl77777 <146434729+jcl77777@users.noreply.github.com>
2024-01-16 18:43:24 +0800

    update to different files

--- replit_day_63.py
+++ replit_day_63.py
@@ -1,13 +1,5 @@
-import random
-
-num = random.randint(10,100)
-
-def countdown():
-  for i in range(num):
-    print(i+1)
-
 #import test's function to be used in main.py
-import test as tt
+import replit_day_63_test as tt
 
 print("Countdown")
 tt.countdown()
